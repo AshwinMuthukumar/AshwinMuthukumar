199b17afd7424458e2148e5c88f738747144e74d
AshwinMuthukumar <168209461+AshwinMuthukumar@users.noreply.github.com>
2024-05-03 10:46:30 +0530

    Add files via upload

---
+++ Decentralized Voting Application/scripts/deploy.js
@@ -0,0 +1,17 @@
+
+const hre = require("hardhat");
+
+async function main() {
+  
+  const Voting = await hre.ethers.getContractFactory("Voting");
+  const Voting_ = await Voting.deploy(["Mark","Deacon","Jack"],20);
+  await Voting_.deployed();
+  console.log(
+    `Contract address : ${Voting_.address}`
+  );
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
